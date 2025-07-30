bf8af69840ce8ba5dd126a0efbeaddc88a0089a7
kamero0n <cagomez@usc.edu>
2025-07-30 14:52:21 -0700

    fixed flashing thing

--- main.lua
+++ main.lua
@@ -26,11 +26,14 @@ function love.update(dt)
 end
 
 function love.mousepressed(x, y, button, istouch)
-    isMouseDragging = true
-
     if button == 1 then
+        isMouseDragging = true
+
         firstCorner.x = x
         firstCorner.y = y
+
+        secondCorner.x = x
+        secondCorner.y = y
     end
 end
 
@@ -49,10 +52,7 @@ end
 function love.draw()
     -- set target canvas
     love.graphics.setCanvas(gameCanvas)
-    love.graphics.clear()
-
-    love.graphics.setColor(0, 0, 0, 1) -- set to black
-    love.graphics.rectangle("fill", 0, 0, WINDOWWIDTH, WINDOWHEIGHT) 
+    love.graphics.clear(0, 0, 0, 1)
 
     love.graphics.setColor(1, 1, 1, 1) -- set to white
 
