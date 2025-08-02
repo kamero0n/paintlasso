1c311598570c910ec50e8517e5bef765dc8433d0
Cameron Gomez <camigogo6@gmail.com>
2025-08-02 09:43:07 -0700

    shelf items + locations added

--- assets/levels/level2.lua
+++ assets/levels/level2.lua
@@ -26,6 +26,36 @@ function Level2.init(world)
     table.insert(items, SelectableObject(150, WINDOWHEIGHT - 330, 30, 30, {0.8, 0.2, 0.2}, world))
     table.insert(items, SelectableObject(200, WINDOWHEIGHT - 335, 25, 35, {0.2, 0.8, 0.2}, world))
     table.insert(items, SelectableObject(250, WINDOWHEIGHT - 340, 35, 40, {0.2, 0.2, 0.8}, world))
+
+    -- zone for item placement
+    table.insert(targetZones, {
+        x = 365,
+        y = WINDOWHEIGHT - 460,
+        width = 30,
+        height = 30,
+        color = {0.8, 0.2, 0.2},
+        itemType = 1,
+        filled = false
+    })
+    table.insert(targetZones, {
+        x = 530,
+        y = WINDOWHEIGHT - 415,
+        width = 25,
+        height = 35,
+        color = {0.2, 0.8, 0.2},
+        itemType = 2,
+        filled = false
+    })
+    table.insert(targetZones, {
+        x = 460,
+        y = WINDOWHEIGHT - 370,
+        width = 35,
+        height = 40,
+        color = {0.2, 0.2, 0.8},
+        itemType = 3,
+        filled = false
+    })
+
 end
 
 local function checkDist(obj1, obj2, threshold)
@@ -37,6 +67,17 @@ local function checkDist(obj1, obj2, threshold)
     local dist = math.sqrt((obj1CenterX - obj2CenterX)^2 + (obj1CenterY - obj2CenterY)^2)
 
     return dist < threshold
+end
+
+local function checkIfObjIsDragged(obj, selectedObjects, lasso_state, isMouseDragging)
+    local isBeingDragged = false
+    for j, selectedOBj in ipairs(selectedObjects) do
+        if selectedOBj == obj and lasso_state == "dragging" and isMouseDragging then
+                isBeingDragged = true
+        end
+    end
+
+    return isBeingDragged
 end
 
 function Level2.play(player, dt, selectedObjects, lasso_state, isMouseDragging, allObjects)
@@ -63,6 +104,12 @@ function Level2.draw()
 
     -- draw employee
     employee:draw()
+
+    -- draw target zones for items
+    for _, zone in ipairs(targetZones) do
+        love.graphics.setColor(zone.color[1], zone.color[2], zone.color[3], zone.filled and 0.1 or 0.3)
+        love.graphics.rectangle("fill", zone.x, zone.y, zone.width, zone.height)
+    end
 
     -- draw items
     for _, item in ipairs(items) do
