3c75957078437cc82d6bd96046208880d02b39dc
kamero0n <cagomez@usc.edu>
2025-08-02 02:19:45 -0700

    mr employee added

--- assets/levels/level2.lua
+++ assets/levels/level2.lua
@@ -8,7 +8,7 @@ local WINDOWWIDTH, WINDOWHEIGHT = love.graphics.getDimensions()
 -- first puzzle stuff
 local employee
 local shelfStacked = false
-
+local employeeBlockRadius = 70
 
 function Level2.init(world)
     -- create ground collider
@@ -16,16 +16,41 @@ function Level2.init(world)
     ground:setType('static')
 
     -- create employee
-    employee = NPC(300, WINDOWHEIGHT - 330, 25, 15, {0.2, 0.2, 0.2}, 0)
+    employee = NPC(400, WINDOWHEIGHT - 380, 40, 80, {0.2, 0.4, 0.8}, 0)
+end
+
+local function checkDist(obj1, obj2, threshold)
+    local obj1CenterX = obj1.x + obj1.width/2
+    local obj1CenterY = obj1.y + obj1.height/2
+    local obj2CenterX = obj2.x + obj2.width/2
+    local obj2CenterY = obj2.y + obj2.height/2
+
+    local dist = math.sqrt((obj1CenterX - obj2CenterX)^2 + (obj1CenterY - obj2CenterY)^2)
+
+    return dist < threshold
 end
 
 function Level2.play(player, dt, selectedObjects, lasso_state, isMouseDragging, allObjects)
+    -- update employee
+    employee:update(dt)
+
+    -- block player with employee until items are stocked
+    if not shelfStacked and checkDist(player, employee, employeeBlockRadius) then
+        local employeeCenterX = employee.x + employee.width/2
+        player.x = employeeCenterX - employeeBlockRadius - player.width / 2
+    end
+
+    
+
 end
 
 function Level2.draw()
     -- floor
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.rectangle("fill", 0, WINDOWHEIGHT - 300, WINDOWWIDTH * 4, 300)
+
+    -- draw employee
+    employee:draw()
 
 end
 
