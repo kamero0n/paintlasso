ebc6d1fd9631744e2fc47e34776a635d7257a051
kamero0n <cagomez@usc.edu>
2025-08-02 00:03:03 -0700

    please work

--- assets/levels/level2.lua
+++ assets/levels/level2.lua
@@ -5,11 +5,18 @@ Level2 = {}
 -- window stuff
 local WINDOWWIDTH, WINDOWHEIGHT = love.graphics.getDimensions()
 
+-- first puzzle stuff
+local employee
+local shelfStacked = false
+
 
 function Level2.init(world)
     -- create ground collider
     ground = world:newRectangleCollider(0, WINDOWHEIGHT - 300, WINDOWWIDTH * 4, 300)
     ground:setType('static')
+
+    -- create employee
+    employee = NPC(300, WINDOWHEIGHT - 330, 25, 15, {0.2, 0.2, 0.2}, 0)
 end
 
 function Level2.play(player, dt, selectedObjects, lasso_state, isMouseDragging, allObjects)
